f03eab95907ac8f17b8cb5409aa8325c2fb21ab5
TSCP6 <1284127668@qq.com>
2025-10-20 17:26:16 +0800

    Finish basic findWay function, enemy could move to a static target place

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    MapManager mapManager;
+
+    public float moveSpeed = 5f;
+    public float movementThreshold = 0.01f;
+    public bool showPath = false;
+
+    public GameObject target;
+
+    bool isMoving = false;
+
+    private List<MapManager.Node> curPath;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //在场景中寻找第一个含有mapmanager组件的物体，并赋值组件的引用
+        mapManager = FindObjectOfType<MapManager>();
+
+        StartCoroutine(WaitForMapAndMove());
+    }
+
+    IEnumerator WaitForMapAndMove()
+    {
+        while (mapManager == null || mapManager.grid == null)
+        {
+            yield return null;
+        }
+        if (target != null)
+        {
+            MoveToTarget(target.transform.position);
+        }
+    }
+
+    void MoveToTarget(Vector2 targetPos)
+    {
+        if (isMoving) return; //正在寻路则不做处理
+
+        List<MapManager.Node> path = mapManager.FindPath(transform.position, targetPos);
+
+        if (path != null && path.Count > 0)
+        {
+            StartCoroutine(FollowPath(path));
+            curPath = path;
+        }
+        else
+        {
+            Debug.Log("can't find the path");
+        }
+    }
+
+    IEnumerator FollowPath(List<MapManager.Node> path)
+    {
+        isMoving = true;
+
+        foreach (MapManager.Node node in path)
+        {
+            while (Vector2.Distance(transform.position, node.worldPosition) > movementThreshold)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, node.worldPosition, moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+        }
+
+        isMoving = false;
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        if (mapManager == null)
+        {
+            mapManager = FindObjectOfType<MapManager>();
+        }
+        if (mapManager != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(transform.position, Vector2.one * mapManager.gridSize);
+        }
+        else
+        {
+            // 如果mapManager还是null,使用默认大小
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawCube(transform.position, Vector2.one);
+        }
+        if (showPath)
+        {
+            if(curPath  != null)
+            {
+                foreach(MapManager.Node node in curPath)
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawCube(node.worldPosition, mapManager.gridSize * Vector2.one);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 
@@ -11,7 +12,7 @@ public class MapManager : MonoBehaviour
 
     public Vector2 gridOrigin = Vector2.zero;
 
-    public LayerMask obstacleLayer ;
+    public LayerMask obstacleLayer;
 
     public Node[,] grid;
 
@@ -26,19 +27,20 @@ public class MapManager : MonoBehaviour
         public Node parent; //回溯路径
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         CreateGrid();
     }
+
+    //-------------------初始化逻辑--------------------
 
     void CreateGrid()
     {
         grid = new Node[gridWidth, gridHeight];
 
-        for (int x=0; x<gridWidth; x++)
+        for (int x = 0; x < gridWidth; x++)
         {
-            for(int y=0; y<gridHeight; y++)
+            for (int y = 0; y < gridHeight; y++)
             {
                 Node node = new Node();
                 node.position = new Vector2Int(x, y); //表格坐标赋值
@@ -57,8 +59,8 @@ public class MapManager : MonoBehaviour
 
     public Vector2Int WorldToGrid(Vector2 worldPosition) //世界坐标转化为网格坐标
     {
-        int x = Mathf.RoundToInt((worldPosition.x - gridOrigin.x) / gridSize);
-        int y = Mathf.RoundToInt((worldPosition.y - gridOrigin.y) / gridSize);
+        int x = Mathf.RoundToInt((worldPosition.x - gridOrigin.x - gridSize * 0.5f) / gridSize);
+        int y = Mathf.RoundToInt((worldPosition.y - gridOrigin.y - gridSize * 0.5f) / gridSize);
         return new Vector2Int(Mathf.Clamp(x, 0, gridWidth - 1), Mathf.Clamp(y, 0, gridHeight - 1));
         //Clamp函数判断x是否在最小值0到最大值width之间，是则返回第一个参数
     }
@@ -66,7 +68,7 @@ public class MapManager : MonoBehaviour
     public Vector2 GridToWorld(Vector2Int gridPosition)
     {
         return new Vector2(
-            gridOrigin.x + gridPosition.x * gridSize + gridSize * 0.5f, 
+            gridOrigin.x + gridPosition.x * gridSize + gridSize * 0.5f,
             gridOrigin.y + gridPosition.y * gridSize + gridSize * 0.5f);
     }
 
@@ -81,11 +83,11 @@ public class MapManager : MonoBehaviour
 
     public void OnDrawGizmos() //将表格画在scene中
     {
-        if(grid == null) return;
+        if (grid == null) return;
 
-        for (int x = 0; x < gridWidth; x++) 
+        for (int x = 0; x < gridWidth; x++)
         {
-            for(int y = 0; y < gridHeight; y++)
+            for (int y = 0; y < gridHeight; y++)
             {
                 Node node = grid[x, y];
                 if (node != null)
@@ -100,9 +102,137 @@ public class MapManager : MonoBehaviour
         }
     }
 
+    //-------------------寻路算法函数部分-------------------
+    public List<Node> FindPath(Vector2 startWorldPos, Vector2 endWorldPos) //寻路方法
+                                                                           //1.转化坐标，获取始终点，并检查结点合法，创建开放列表和关闭哈希表，将起点添加开放表
+                                                                           //2.循环开始，在所有开放表中找到f值最少的或者g值最小的，添加到关闭表，移除开放表
+                                                                           //3.如果找到终点直接回溯，查找所有相邻结点，忽略障碍物或者已经在关闭列表邻居，
+                                                                           //4.重新计算g值，为当前g值加上当前到邻居的距离，如果新的g值小于原有的值或者不在开放列表中，重新赋值g，h值，设置父节点，如果开放列表不包含，则添加进去
+                                                                           //5.循环结束没有找到就返回空
+    {
+        Vector2Int startGrid = WorldToGrid(startWorldPos);
+        Vector2Int endGrid = WorldToGrid(endWorldPos);
+
+        Node startNode = GetNode(startGrid.x, startGrid.y);
+        Node endNode = GetNode(endGrid.x, endGrid.y);
+
+        if (startNode == null || endNode == null)
+        {
+            Debug.Log("Start or end point is null");
+            return null;
+        }
+
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                grid[x, y].gCost = float.MaxValue;
+                grid[x, y].hCost = 0;
+                grid[x, y].parent = null;
+            }
+        }
+
+        List<Node> openList = new List<Node>();
+        HashSet<Node> closeList = new HashSet<Node>();
+
+        startNode.gCost = 0; ;
+        startNode.hCost = GetDistance(startNode, endNode);
+
+        openList.Add(startNode);
+
+        while (openList.Count > 0)
+        {
+            Node curNode = openList[0];
+            for (int i = 1; i < openList.Count; i++)
+            {
+                if (openList[i].fCost < curNode.fCost
+                    || (openList[i].fCost == curNode.fCost && openList[i].hCost < curNode.hCost))
+                {
+                    curNode = openList[i];
+                }
+            }
+
+            openList.Remove(curNode);
+            closeList.Add(curNode);
+
+            if (curNode == endNode) return RetracePath(startNode, endNode);
+
+            foreach (Node neighor in GetNeighbors(curNode))
+            {
+                if (neighor.isObstacle || closeList.Contains(neighor)) continue;
+
+                float newGCost = curNode.gCost + GetDistance(neighor, curNode);
+                if (newGCost < neighor.gCost || !openList.Contains(neighor))
+                {
+                    neighor.gCost = newGCost;
+                    neighor.hCost = GetDistance(neighor, endNode);
+                    neighor.parent = curNode;
+
+                    if (!openList.Contains(neighor))
+                    {
+                        openList.Add(neighor);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    List<Node> RetracePath(Node startNode, Node endNode) //从终点回溯到起点
+    {
+        List<Node> path = new List<Node>();
+        Node curNode = endNode;
+
+        while (curNode != startNode)
+        {
+            path.Add(curNode);
+            curNode = curNode.parent;
+        }
+
+        path.Reverse();
+        return path;
+    }
+
+    List<Node> GetNeighbors(Node node) //获取邻居节点
+    {
+        List<Node> neighbors = new List<Node>();
+
+        for (int x = -1; x <= 1; x++)
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0) continue; //跳过自己
+                int nearX = node.position.x + x;
+                int nearY = node.position.y + y;
+                Node neighbor = GetNode(nearX, nearY);
+                if (neighbor != null)
+                {
+                    if (x != 0 && y != 0) //检测对角线防止穿墙
+                    {
+                        Node sideX = GetNode(node.position.x + x, node.position.y);
+                        Node sideY = GetNode(node.position.x, node.position.y + y);
+
+                        //跳过对角线邻居，这里也可以设置两边都是障碍时不可通过
+                        if ((sideX != null && sideX.isObstacle) || (sideY != null && sideY.isObstacle)) continue;
+                    }
+                    neighbors.Add(neighbor);
+                }
+            }
+        return neighbors;
+    }
+
+    float GetDistance(Node nodeA, Node nodeB) //两个结点之间的距离
+    {
+        int disX = Mathf.Abs(nodeA.position.x - nodeB.position.x);
+        int disY = Mathf.Abs(nodeA.position.y - nodeB.position.y);
+
+        //对角线值为14，直线为10，对角线长度最多的为对应距离
+        if (disX > disY) return 14 * disY + 10 * (disX - disY);
+        return 14 * disX + 10 * (disY - disX);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
